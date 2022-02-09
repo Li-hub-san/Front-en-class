cd25e2003a0b16e27b9ccb34ec0da80ae573ea7d
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-02-09 13:05:54 +0000

    Aula 14;

---
+++ learning-javascript/aula14/README.md
@@ -0,0 +1,5 @@
+# Aula 14
+
+<br> let - can be reassigned (function scope);
+<br> const - cannot be reassigned (global scope)
+<br> var - permite usar uma variavel e então declara-la.
---
+++ learning-javascript/aula14/script.js
@@ -0,0 +1,21 @@
+let a = 10;
+a = "hello";
+console.log(a);
+
+var b = 5;
+b = 20;
+console.log(b);
+
+let c;
+c = 21;
+console.log("let:" + c);
+
+function doSomething() {
+    const numero1 = 5;
+
+    if (numero1 > 0) {
+        console.log("A minha variavel é maior que 0.")
+    }
+}
+
+console.log(doSomething());
