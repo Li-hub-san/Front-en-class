f8e9b26c4405e519e34d64d97b0fcfb659f8007d
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-02-15 23:01:48 +0000

    Refactored all files and separated by language.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Front-end-class
 
 <br>
-[Class 3](https://li-hub-san.github.io/front-end-class/)
-[Class 3](https://li-hub-san.github.io/front-end-class/)
+Class 3https:[http://li-hub-san.github.io/front-end-class/]
 
 Início JavaScript:
 ajax: spa (quando recebe o conteudo apenas, normalmente é um pedido ajux)
@@ -10,14 +9,13 @@ JQuery: biblioteca de JavaScript
 
 Babel,Vue, Webpack: transpilar o codigo do ES6 para o ES5
 
-Assembly: Linguagem nativa para correr no browser que recebe todas as linguagens e vai compilar para a linguagem que pretendemos.
+Assembly: Linguagem nativa para correr no browser que recebe todas as linguagens e vai compilar para a linguagem que
+pretendemos.
 
-JavaScript
-O correcto é colocar o script no topo, e dar a informação para ele correr apenas depois de carregar a página.
+JavaScript O correcto é colocar o script no topo, e dar a informação para ele correr apenas depois de carregar a página.
 
 JQuery biblioteca (ver integrity).
 
-Colocar ficheiro de JQuery no html,
-paragrafo e botão e clicando no botão ele apagar o paragrafo.
+Colocar ficheiro de JQuery no html, paragrafo e botão e clicando no botão ele apagar o paragrafo.
 
 possivel fazer jogos com biblioteca JQuery: usando sprites e posiçoes.
--- learning-javascript/aula14/script.js
+++ learning-javascript/aula14/script.js
@@ -1,37 +1,41 @@
-// let a = 10;
-// a = "hello";
-// console.log(a);
-//
-// var b = 5;
-// b = 20;
-// console.log(b);
-//
-// let c;
-// c = 21;
-// console.log("let:" + c);
-//
-// function doSomething() {
-//     const numero1 = 5;
-//
-//     if (numero1 > 0) {
-//         console.log("A minha variavel é maior que 0.")
-//     }
-// }
-//
-// console.log(doSomething());
+/**
+ * Removes all <p> tags with an odd innerText.
+ */
+function removeOddParagraphTags() {
+  const pTags = document.getElementsByTagName('p');
 
-const pTag =document.getElementsByTagName("p");
-const buttonTag = document.getElementsByTagName("button");
-
-function removeParagraphTags(){
-
-    for (let i = 0; i < 4; i++) {
-        let currentTag = pTag[i];
-        console.log(currentTag);
-
-        if (currentTag.innerHTML % 2 !== 0) {
-            currentTag.remove();
-        }
+  for (let i = pTags.length - 1; i >= 0; i--) {
+    let currentTag = pTags[i];
+    let isOdd = +currentTag.innerHTML % 2 !== 0;
+    if (isOdd && !isNaN(+currentTag.innerHTML)) {
+      currentTag.remove();
     }
+  }
+}
 
+function printInfo() {
+  const infoEl = document.getElementById('info');
+
+  const lista = {
+    alunos: [
+      { nome: 'Alda', idade: 25 },
+      { nome: 'Ivone', idade: 30 },
+      { nome: 'Ana', idade: 33 },
+    ],
+  };
+
+  let output = '';
+  for (let i = 0; i < lista.alunos.length; i++) {
+    let aluno = lista.alunos[i];
+
+    let properties = `
+      <div class="aluno">
+        <p>nome: ${aluno.nome}</p>
+        <p>idade: ${aluno.idade}</p>
+      </div>`;
+
+    output += properties;
+  }
+
+  infoEl.innerHTML = output;
 }
