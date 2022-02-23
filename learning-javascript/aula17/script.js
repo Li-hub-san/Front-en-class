876b5e13cd6644e1079a994d86fc66b6e95ef77c
Li-hub-san <ligia.baptista.santos@gmail.com>
2022-02-23 13:07:47 +0000

    Aula 17, added node.js to project.

--- learning-javascript/aula17/script.js
+++ learning-javascript/aula17/script.js
@@ -1,22 +1,21 @@
-// document.getElementById("demo").innerHTML = ages.filter(checkAdult);
-//
-// function checkAdult(age) {
-//     return age >= 18;
-// }
-
-function teste() {
-    const ages = [1, 2, 3, 4, 5, 6, 32, 33, 16, 40, 12, 52, 36, 10];
-
+function verificarMaiorDeIdade() {
+    const ages = [1, 2, 3, 4, 5, 6, 32, 33, 16, 40, 12, 52, 36, 10, "22"];
     const pEl = document.getElementById("demo");
     const output = [];
 
-    for (let i = 8; i < ages.length; i++) {
+    for (let i = 0; i < ages.length; i++) {
         let currentEl = ages[i];
 
-        if (currentEl >= 18) {
+        if (checkAdult(currentEl)) {
             output.push(currentEl);
         }
 
         pEl.innerHTML = output.toString();
     }
+}
+
+
+
+function checkAdult(age) {
+    return age >= 18;
 }
